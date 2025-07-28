5e2f6e9fb26b40ffa9a0d823027e0841b87f89f5
amin <amin1382422amin@gmail.com>
2025-07-28 14:52:58 +0330

    added the foldering

---
+++ lib/features/daily_news/domain/use_cases/get_article.dart
@@ -0,0 +1,14 @@
+import 'package:my_news_app/core/resources/data_state.dart';
+import 'package:my_news_app/core/use_cases/use_case.dart';
+import 'package:my_news_app/features/daily_news/data/models/article_model.dart';
+import 'package:my_news_app/features/daily_news/data/repository/article_repository.dart';
+
+class GetArticle implements UseCase<DataState<List<ArticleModel>>, void> {
+  const GetArticle(this._articleRepository);
+
+  final ArticleRepository _articleRepository;
+
+  @override
+  Future<DataState<List<ArticleModel>>> call({void params}) =>
+      _articleRepository.getMany();
+}
---
+++ lib/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart
@@ -0,0 +1,27 @@
+import 'package:dio/dio.dart';
+
+import 'package:equatable/equatable.dart';
+import 'package:my_news_app/features/daily_news/domain/entities/article.dart';
+
+class RemoteArticleState extends Equatable {
+  final List<Article>? articles;
+  final DioException? exception;
+
+  const RemoteArticleState({this.articles, this.exception});
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [articles!, exception!];
+}
+
+class RemoteDataLoading extends RemoteArticleState {
+  const RemoteDataLoading() : super();
+}
+
+class RemoteDataDone extends RemoteArticleState {
+  const RemoteDataDone(List<Article> articles) : super(articles: articles);
+}
+
+class RemoteDataError extends RemoteArticleState {
+  const RemoteDataError(DioException ex) : super(exception: ex);
+}
